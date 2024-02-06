78f9a901e618dc277342124f9f455cdee0fe56b1
Woyken <13166802+Woyken@users.noreply.github.com>
2024-02-06 17:19:50 +0200

    Migrate setuptools to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as readmeFile:
-    long_description = readmeFile.read()
-
-setuptools.setup(
-    author="Woyken",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Home Automation",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    description="Electrolux OneApp OCP API",
-    install_requires=["aiohttp"],
-    keywords="home automation electrolux aeg ocp oneapp api",
-    license="MIT License",
-    long_description_content_type="text/markdown",
-    long_description=long_description,
-    name="pyelectroluxocp",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    url="https://github.com/Woyken/py-electrolux-ocp",
-    version="0.0.9",
-    zip_safe=False,
-)
+setup()
